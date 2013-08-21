b56500333429fdfea6cfd0117f78413a68a31b14
ch604 <walilkoa@gmail.com>
2013-08-21 13:36:04 -0400

    Update pleskinitialsync.sh
    updated pleskhosts.sh to match version in git

--- pleskinitialsync.sh
+++ pleskinitialsync.sh
@@ -907,7 +907,7 @@ hostsfile() { #Generate a hosts file on the target machine
 echo -e "${purple}Generating hosts file...${noclr}"
 cat > /var/pleskhosts.sh <<'EOF'
 #!/bin/bash
-#awalilko@lw 09/06/12
+#awalilko@lw 01/15/13
 hostsfile=/var/www/vhosts/default/htdocs/hostsfile.txt
 althostsfile=/var/www/vhosts/default/htdocs/hosts.txt
 ip=`/usr/local/psa/bin/ipmanage -l | grep " S " | head -n1 | cut -d\: -f2 | cut -d\/ -f1`
@@ -925,10 +925,13 @@ for domain in `mysql psa -u admin -p$(cat /etc/psa/.psa.shadow) -Ns -e 'select n
   echo $domip  $domain www.$domain >> $hostsfile;
 done
 echo "Done!"
-#create one line per ip
+#create one line per ip for ipv4, then ipv6
 echo "Generating alternate file..."
-for domip in `cat $hostsfile | cut -d\  -f1 | sort | uniq | grep -v [a-zA-Z]`; do
-  echo $domip `cat $hostsfile | grep $domip | cut -d\  -f2-3 | tr '\n' ' '` >> $althostsfile;
+for domip in `cat $hostsfile | cut -d\ -f1 | sort | uniq | grep -v [a-zA-Z]`; do
+echo $domip `cat $hostsfile | grep $domip | cut -d\ -f2-3 | tr '\n' ' '` >> $althostsfile;
+done
+for domip in `cat $hostsfile | cut -d\ -f1 | sort | uniq | grep :`; do
+echo $domip `cat $hostsfile | grep $domip | cut -d\ -f2-3 | tr '\n' ' '` >> $althostsfile;
 done
 echo "Done!"
 #correct server to accept requests by IP
@@ -939,6 +942,21 @@ if [ -s /etc/httpd/modsecurity.d ]; then
   echo "ModSec corrected! All Set!"
 else
   echo "ModSec not found! All set!"
+fi
+#determine if there is a default domain and place the files there
+echo "Ensuring that the correct folder is used for the default IP..."
+domainfolder=`mysql psa -u admin -p$(cat /etc/psa/.psa.shadow) -Ns -e "select d.name from domains AS d join IP_Addresses as ipa where ipa.ip_address='$ip' and d.id=ipa.default_domain_id"`
+if [[ $domainfolder ]]; then
+  cp $hostsfile /var/www/vhosts/$domainfolder/httpdocs/
+  cp $althostsfile /var/www/vhosts/$domainfolder/httpdocs/
+  echo "Copied files to default domain $domainfolder!"
+else
+echo "Server default folder should work!"
+fi
+#determine if there is a hostname account and copy the files to that docroot
+if [ -d /var/www/vhosts/$(hostname) ]; then
+  cp $hostsfile /var/www/vhosts/$(hostname)/httpdocs/
+  cp $althostsfile /var/www/vhosts/$(hostname)/httpdocs/
 fi
 #output resulting file link
 echo
