6361478be124770892479c2c619c4588ccda64d1
ch604 <walilkoa@gmail.com>
2013-10-29 13:03:59 -0400

    increment $ver

--- pleskinitialsync.sh
+++ pleskinitialsync.sh
@@ -2,7 +2,7 @@
 #Plesk migration script
 #by awalilko@liquidweb.com
 #many thanks to abrevick@lw for his cpanel initialsync script, off of which this was based.
-ver='oct 28.13'
+ver='oct 29.13'
 #=========================================================
 #initial setup and global variables
 #==================================
